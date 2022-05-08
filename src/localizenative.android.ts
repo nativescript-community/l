50e2a41d607e25b01a8d39164fb95c045e310a0e
Martin Guillon <martin.guillon@akylas.fr>
2022-05-08 21:49:47 +0200

    fix(android): N 8.2 fix

--- src/localizenative.android.ts
+++ src/localizenative.android.ts
@@ -1,21 +1,18 @@
 import { vsprintf } from 'sprintf-js';
-import * as utils from '@nativescript/core/utils/utils';
-import { Application } from '@nativescript/core';
+import { Application, Device, Utils} from '@nativescript/core';
 
-const getResources = (function() {
-    let resources = null;
-    return function() {
-        if (resources === null) {
-            resources = utils.ad.getApplicationContext().getResources();
-        }
-        return resources;
-    };
-})();
+let resources: android.content.res.Resources;
+function getResources() {
+    if (!resources) {
+        resources = (Utils.ad.getApplicationContext() as android.content.Context).getResources();
+    }
+    return resources;
+};
 
 export function localizeNative(key: string, ...args: string[]): string {
     let localizedString;
     try {
-        const identifier = utils.ad.resources.getStringId(key);
+        const identifier = Utils.ad.resources.getStringId(key);
         localizedString = identifier === 0 ? key : getResources().getString(identifier);
         return vsprintf(localizedString, args);
     } catch (error) {
@@ -23,15 +20,19 @@ export function localizeNative(key: string, ...args: string[]): string {
     }
 }
 
+const sdkVersion = parseInt(Device.sdkVersion, 10);
 
 export function overrideNativeLocale(lang: string): boolean {
-    const locale = new java.util.Locale(lang.substring(0, 2));
+    const locale = new java.util.Locale(lang);
     java.util.Locale.setDefault(locale);
 
-    const resources = Application.android.context.getResources();
+    const resources = getResources();
     const configuration = resources.getConfiguration();
     configuration.locale = locale;
-
-    resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+    if (sdkVersion < 17) {
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+    } else {
+        (Utils.ad.getApplicationContext() as android.content.Context).createConfigurationContext(configuration);
+    }
     return true;
 }
