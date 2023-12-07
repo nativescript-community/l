a4d1da49bf8236ec2271e4a9171b682de63b4f9b
farfromrefuge <>
2023-12-07 16:47:07 +0100

    chore: tsc

--- src/hooks/before-syncChangedDataOnDevices.ts
+++ src/hooks/before-syncChangedDataOnDevices.ts
@@ -23,41 +23,41 @@ module.exports =  function (
     // in most case. For edge case a rebuild is needed
     return;
 
-    const changedFiles = hookArgs?.data?.files;
-    let shouldIgnore = true;
-    if (changedFiles?.length) {
-            changedFiles.forEach(changedFile => {
-            if (/i18n/.test(changedFile)) {
-                shouldIgnore = false;
-            }
-        })
-    }
-    if (shouldIgnore) {
-            return;
-        }
-    const platformName = (
-        (hookArgs && hookArgs.platformData && hookArgs.platformData.normalizedPlatformName) ||
-        (hookArgs.checkForChangesOpts && hookArgs.checkForChangesOpts.platform) ||
-        ''
-    ).toLowerCase();
+    // const changedFiles = hookArgs?.data?.files;
+    // let shouldIgnore = true;
+    // if (changedFiles?.length) {
+    //         changedFiles.forEach(changedFile => {
+    //         if (/i18n/.test(changedFile)) {
+    //             shouldIgnore = false;
+    //         }
+    //     })
+    // }
+    // if (shouldIgnore) {
+    //         return;
+    //     }
+    // const platformName = (
+    //     (hookArgs && hookArgs.platformData && hookArgs.platformData.normalizedPlatformName) ||
+    //     (hookArgs.checkForChangesOpts && hookArgs.checkForChangesOpts.platform) ||
+    //     ''
+    // ).toLowerCase();
 
-    projectData =
-        hookArgs && (hookArgs.projectData || (hookArgs.checkForChangesOpts && hookArgs.checkForChangesOpts.projectData));
+    // projectData =
+    //     hookArgs && (hookArgs.projectData || (hookArgs.checkForChangesOpts && hookArgs.checkForChangesOpts.projectData));
 
-    const platformData = getPlatformData(hookArgs && hookArgs.platformData, projectData, platformName, injector);
-    let converter: ConverterCommon;
-    const dataProvider = new DataProvider(logger, projectData);
+    // const platformData = getPlatformData(hookArgs && hookArgs.platformData, projectData, platformName, injector);
+    // let converter: ConverterCommon;
+    // const dataProvider = new DataProvider(logger, projectData);
 
-    if (platformName === 'android') {
-        converter = new ConverterAndroid(dataProvider, androidResourcesMigrationService, logger, platformData, projectData);
-    } else if (platformName === 'ios') {
-        converter = new ConverterIOS(dataProvider, logger, platformData, projectData);
-    } else {
-        logger.warn(`Platform '${platformName}' isn't supported: skipping localization`);
-        return;
-    }
+    // if (platformName === 'android') {
+    //     converter = new ConverterAndroid(dataProvider, androidResourcesMigrationService, logger, platformData, projectData);
+    // } else if (platformName === 'ios') {
+    //     converter = new ConverterIOS(dataProvider, logger, platformData, projectData);
+    // } else {
+    //     logger.warn(`Platform '${platformName}' isn't supported: skipping localization`);
+    //     return;
+    // }
 
-    converter.run();
+    // converter.run();
 };
 
 function getPlatformData(
